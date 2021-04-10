361dba6f7a23c7e088b1211404e99c9404acc314
Stuart Harrison <stuart@itecassist.co.za>
2021-04-10 13:07:30 +0200

    add bootstrap and auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile', 10)->unique();
             $table->string('password');
+            $table->tinyInteger('is_Admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
